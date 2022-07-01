6181440cc119a0f9685f952171ab803967aad517
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-07-01 21:59:21 +0545

    Add radio image control

---
+++ customind/src/assets/js/controls/radio-image/radio-image.js
@@ -0,0 +1,48 @@
+import { memo, useState } from '@wordpress/element';
+import { Tooltip } from '../../components';
+
+const RadioImage = ( props ) => {
+	const [ value, setValue ] = useState( props.setting.get() || '' );
+
+	const {
+		label,
+		description,
+		choices,
+		col = 2,
+	} = props.params;
+
+	const update = ( val ) => {
+		setValue( val );
+		props.setting.set( val );
+	};
+
+	return (
+		<div className="customize-control customize-control-customind-sortable">
+			{ label && (
+				<div className="customind-control-head">
+					<span className="customize-control-title">{ label }</span>
+					{ description && (
+						<Tooltip>
+							<span className="customize-control-description">{ description }</span>
+						</Tooltip>
+					) }
+				</div>
+			) }
+			<div className="customind-control-body" style={ { '--customind-col': col } }>
+				{ Object.entries( choices ).map( ( [ k, v ] ) => (
+					<div role="button" tabIndex={ 0 } onClick={ () => update( k ) } onKeyDown={ e => {
+						if ( e.code === 'Enter' ) {
+							update( k );
+						}
+					} } key={ k } data-id={ k } className={ `customind-radio-image${ value === k ? ' active' : '' }` } data-label={ v?.label || null }>
+						{ v?.url && (
+							<img style={ { width: '100%' } } src={ v.url } alt={ v?.label || k } />
+						) }
+					</div>
+				) ) }
+			</div>
+		</div>
+	);
+};
+
+export default memo( RadioImage );

cd5b7f1715f32d5384b13faa3e246385a1f3dc8f
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-07-19 18:39:45 +0545

    Remove unnecessary scripts and format code

--- customind/src/assets/js/controls/utils.js
+++ customind/src/assets/js/controls/utils.js
@@ -12,66 +12,27 @@ export const defineControls = ( type, Component ) => {
 
 			wp.customize.Control.prototype.initialize.call( control, id, params );
 			control.container[ 0 ].classList.remove( 'customize-control' );
+
 			if ( ! args.params?.content ) {
 				args.params.content = $( '<li></li>', {
 					id: `customize-control-${ id.replace( /]/g, '' ).replace( /\[/g, '-' ) }`,
 					class: `customize-control customize-control-${ args.params.type }`,
 				} );
 			}
+
 			control.propertyElements = [];
 			wp.customize.Control.prototype.initialize.call( control, id, args );
 		},
-		_setUpSettingRootLinks() {
-			const control = this,
-				nodes = control.container.find( '[data-customize-setting-link]' );
-
-			$.each( nodes, function() {
-				const node = $( this ),
-					propName = node.data( 'customizeSettingLink' );
-				wp.customize( propName, setting => {
-					const el = new wp.customize.Element( node );
-					control.element.push( el );
-					el.sync( setting );
-					el.set( setting() );
-				} );
-			} );
-		},
-		_setUpSettingPropertyLinks() {
-			const control = this;
-			if ( ! control?.setting ) return;
-			const nodes = control.container.find( '[data-customize-setting-property-link]' );
-
-			$.each( nodes, function() {
-				const node = $( this ),
-					propName = node.data( 'customizeSettingPropertyLink' ),
-					el = new wp.customize.Element( node );
-				control.element.push( el );
-				el.set( control.setting()[ propName ] );
-				el.bind( newPropValue => {
-					let newSetting = control.setting();
-					if ( newPropValue !== newSetting[ propName ] ) {
-						newSetting = _.clone( newSetting );
-						newSetting[ propName ] = newPropValue;
-						control.setting.set( newSetting );
-					}
-				} );
-				control.setting.bind( newValue => {
-					if ( newValue[ propName ] !== el.get() ) {
-						el.set( newValue[ propName ] );
-					}
-				} );
-			} );
-		},
 		ready() {
 			const control = this;
-			control._setUpSettingRootLinks();
-			control._setUpSettingPropertyLinks();
 			wp.customize.Control.prototype.ready.call( control );
-			control.deferred.embedded.done( () => {} );
+			control.deferred.embedded.done();
 		},
 		embed() {
 			const control = this,
 				section = control.section();
+
+			if ( ! section ) return;
 
 			wp.customize.section( section, sec => {
 				if ( sec.expanded() || wp.customize.settings.autofocus.control === control.id ) {
@@ -105,10 +66,4 @@ export const defineControls = ( type, Component ) => {
 	} ) );
 };
 
-export const getObjectValues = ( object ) => {
-	if ( object && 'object' === typeof object ) {
-		return Object.values( object ).map( getObjectValues ).flat();
-	}
-
-	return [ object ];
-};
+export const getObjectValues = ( object ) => object && 'object' === typeof object ? Object.values( object ).map( getObjectValues ).flat() : [ object ];

ce0af719acbb4d90fb5d5095474aa80fda322245
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-07-19 18:36:45 +0545

    Import scss

--- customind/src/assets/js/controls/dimensions/index.js
+++ customind/src/assets/js/controls/dimensions/index.js
@@ -1,5 +1,6 @@
 import { defineControls } from '../utils';
 import Dimensions from './dimensions';
+import './controls.scss';
 
 export default () => {
 	defineControls( 'customind-dimensions', Dimensions );

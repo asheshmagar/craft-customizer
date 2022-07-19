f836eb4fbfbde546714d2e09c6b4ae82a7c480f3
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-07-19 18:35:02 +0545

    Resize previewer based on builder height

--- customind/src/assets/js/controls/builder/builder.js
+++ customind/src/assets/js/controls/builder/builder.js
@@ -1,7 +1,8 @@
-import { memo, useState, useCallback, useEffect } from '@wordpress/element';
+import { memo, useState, useCallback, useEffect, useRef } from '@wordpress/element';
 import { cloneDeep, isEqual } from 'lodash';
-import BuilderPortal from './builder-portal';
 import BuilderArea from './builder-area';
+import { Button } from '@wordpress/components';
+import { usePortal } from '../hooks';
 
 const Builder = ( props ) => {
 	const [ value, setValue ] = useState( props.control.setting.get() || {} );
@@ -10,6 +11,8 @@ const Builder = ( props ) => {
 		areas = {},
 		section,
 	} = props.control.params;
+	const builderRef = useRef();
+	const Portal = usePortal( document.getElementById( 'customize-controls' ) );
 
 	const update = ( row, area, items ) => {
 		const newValue = cloneDeep( value );
@@ -37,38 +40,67 @@ const Builder = ( props ) => {
 			const panel = props.customizer.panel( sec.panel() );
 			if ( panel ) {
 				panel.expanded.bind( isExpanded => {
-					setOpen( !! isExpanded );
+					setOpen( isExpanded );
 				} );
 			}
 		}
 		setOpen( true );
 	}, [] );
 
+	useEffect( () => {
+		const resizePreviewer = () => {
+			if ( ! builderRef.current ) return;
+			const height = builderRef.current?.offsetHeight || '';
+			if ( open ) {
+				props.customizer.previewer.container.css( 'max-height', `calc(100vh - ${ height }px)` );
+				props.customizer.previewer.container.css( {
+					maxHeight: `calc(100vh - ${ height }px)`,
+					marginTop: 0,
+				} );
+			} else {
+				props.customizer.previewer.container.css( {
+					maxHeight: '100vh',
+					marginTop: '0',
+				} );
+			}
+		};
+		resizePreviewer();
+		window.addEventListener( 'resize', resizePreviewer );
+		return () => {
+			window.removeEventListener( 'resize', resizePreviewer );
+		};
+	}, [ open ] );
+
 	return (
 		<div className="customize-control customize-control-customind-builder">
-			<BuilderPortal>
-				<div className={ `customind-builder-wrap${ open ? ' open' : '' }` }>
-					<div className="customind-builder">
-						{ Object.keys( areas ).map( row => (
-							<div key={ row } className={ `customind-builder-areas customind-builder-areas-${ row }` }>
-								{ Object.keys( areas?.[ row ] || {} ).map( ( area ) => {
-									return (
-										<BuilderArea
-											key={ area }
-											className={ `customind-builder-area customind-builder-area-${ area }` }
-											remove={ ( item ) => remove( row, area, item ) }
-											areaItems={ getAreaItems( row, area ) }
-											value={ value }
-											update={ ( val ) => update( row, area, val ) }
-											{ ...props }
-										/>
-									);
-								} ) }
-							</div>
-						) ) }
+			<Portal>
+				<div data-control={ props.control.id } className={ `customind-builder${ open ? ' open' : '' }` }>
+					<div ref={ builderRef } className="customind-builder-rows-wrap">
+						<div className="customind-builder-rows">
+							{ Object.keys( areas ).map( row => (
+								<div key={ row } className={ `customind-builder-row customind-builder-row-${ row }` }>
+									<Button className="customind-builder-row-action" icon="admin-generic" />
+									<div className="customind-builder-areas">
+										{ Object.keys( areas?.[ row ] || {} ).map( ( area ) => {
+											return (
+												<BuilderArea
+													key={ area }
+													className={ `customind-builder-area customind-builder-area-${ area }` }
+													remove={ ( item ) => remove( row, area, item ) }
+													areaItems={ getAreaItems( row, area ) }
+													value={ value }
+													update={ ( val ) => update( row, area, val ) }
+													{ ...props }
+												/>
+											);
+										} ) }
+									</div>
+								</div>
+							) ) }
+						</div>
 					</div>
 				</div>
-			</BuilderPortal>
+			</Portal>
 		</div>
 	);
 };

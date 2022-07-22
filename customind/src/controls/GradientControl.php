dc84ba883e2d4e4e38771fe06eb60fdd31a7c384
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-07-22 15:11:34 +0545

    Add gradient control

---
+++ customind/src/controls/GradientControl.php
@@ -0,0 +1,17 @@
+<?php
+namespace Customind\Control;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * GradientControl.
+ */
+class GradientControl extends BaseControl {
+
+	/**
+	 * Control's type.
+	 *
+	 * @var string
+	 */
+	public $type = 'customind-gradient';
+}

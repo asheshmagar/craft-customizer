84f86ed3d65a021a8fd0f15f1c397b6c85190518
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-08-08 10:27:43 +0545

    Add doc block comments

--- customind/src/BaseOption.php
+++ customind/src/BaseOption.php
@@ -1,12 +1,33 @@
 <?php
+/**
+ * BaseOption class.
+ *
+ * @package Customind
+ */
+
 namespace Customind;
 
+use WP_Customize_Manager;
+
+/**
+ * Base class for Customind options.
+ */
 class BaseOption {
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		add_filter( 'customind_customizer_options', array( $this, 'register_options' ), 10, 2 );
 	}
 
+	/**
+	 * Register options.
+	 *
+	 * @param array $options
+	 * @param WP_Customize_Manager $customize
+	 * @return array
+	 */
 	public function register_options( $options, $customize ) {
 		return $options;
 	}

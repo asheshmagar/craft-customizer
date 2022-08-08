db9594c63951fa6297514dcb4e55e4ad6a5d7c48
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-08-08 10:27:09 +0545

    Add doc block comments

--- customind/src/Sanitize.php
+++ customind/src/Sanitize.php
@@ -1,35 +1,83 @@
 <?php
+/**
+ * Sanitize class.
+ *
+ * @package Customind
+ */
+
 namespace Customind;
 
 use WP_Customize_Setting;
 
+/**
+ * Sanitize class.
+ */
 class Sanitize {
 
+	/**
+	 * Sanitize checkbox.
+	 *
+	 * @param $input
+	 * @return int|string
+	 */
 	public static function sanitize_checkbox( $input ) {
 		return ( ( 1 === $input || '1' === $input || true === (bool) $input ) ? 1 : '' );
 	}
 
+	/**
+	 * Sanitize int.
+	 *
+	 * @param $number
+	 * @param WP_Customize_Setting $setting
+	 * @return int|string
+	 */
 	public static function sanitize_int( $number, $setting ) {
 		return ( is_numeric( $number ) ? intval( $number ) : $setting->default );
 	}
 
+	/**
+	 * Sanitize HTML.
+	 *
+	 * @param $input
+	 * @return string
+	 */
 	public static function sanitize_html( $input ) {
 		return wp_kses_post( $input );
 	}
 
+	/**
+	 * Sanitize no HTML.
+	 *
+	 * @param $input
+	 * @return string
+	 */
 	public static function sanitize_nohtml( $input ) {
 		return wp_filter_nohtml_kses( $input );
 	}
 
+	/**
+	 * Sanitize a string key.
+	 *
+	 * @param $input
+	 * @return string
+	 */
 	public static function sanitize_key( $input ) {
 		return sanitize_key( $input );
 	}
 
+	/**
+	 * Sanitize text field.
+	 *
+	 * @param $input
+	 * @return string
+	 */
 	public static function sanitize_text_field( $input ) {
 		return sanitize_text_field( $input );
 	}
 
 	/**
+	 * Sanitize radio select.
+	 *
 	 * @param $input
 	 * @param WP_Customize_Setting $setting
 	 * @return string
@@ -50,32 +98,43 @@ class Sanitize {
 		return '';
 	}
 
+	/**
+	 * Sanitize alpha color.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return mixed|string
+	 */
 	public static function sanitize_alpha_color( $input, $setting ) {
 		if ( '' === $input ) {
 			return '';
 		}
+
 		if ( 'header_textcolor' === $setting->id && 'blank' === $input ) {
 			return 'blank';
 		}
-		if ( false === strpos( $input, 'rgb' ) ) {
-			return self::sanitize_hex_color( $input );
-		}
+
 		$color = str_replace( ' ', '', $input );
-		if ( strpos( $color, 'rgba' ) !== false ) {
+
+		if ( false !== strpos( $color, 'rgb' ) ) {
 			sscanf( $color, 'rgba(%d,%d,%d,%f)', $red, $green, $blue, $alpha );
-			if ( 'background_color' === $setting->id || 'header_textcolor' === $setting->id ) {
-				return self::convert_rgba_to_hex( $red, $green, $blue, $alpha );
-			}
-			return 'rgba(' . $red . ',' . $green . ',' . $blue . ',' . $alpha . ')';
+			$alpha = $alpha ? $alpha : 1;
+			return self::to_hex( $red, $green, $blue, $alpha );
 		}
-		sscanf( $color, 'rgb(%d,%d,%d)', $red, $green, $blue );
-		if ( 'background_color' === $setting->id || 'header_textcolor' === $setting->id ) {
-			return self::convert_rgba_to_hex( $red, $green, $blue );
-		}
-		return 'rgb(' . $red . ',' . $green . ',' . $blue . ')';
+
+		return self::sanitize_hex_color( $input );
 	}
 
-	public static function convert_rgba_to_hex( $red, $green, $blue, $alpha = 1 ) {
+	/**
+	 * Convert rga to hex.
+	 *
+	 * @param $red
+	 * @param $green
+	 * @param $blue
+	 * @param $alpha
+	 * @return string
+	 */
+	public static function to_hex( $red, $green, $blue, $alpha = 1 ) {
 		$red   = dechex( (int) $red );
 		$green = dechex( (int) $green );
 		$blue  = dechex( (int) $blue );
@@ -96,15 +155,27 @@ class Sanitize {
 			} else {
 				$alpha = dechex( $alpha );
 			}
-			return $red . $green . $blue . $alpha;
+			return "#$red$green$blue$alpha";
 		}
-		return $red . $green . $blue;
+		return "#$red$green$blue";
 	}
 
+	/**
+	 * Sanitize false values.
+	 *
+	 * @return false
+	 */
 	public static function sanitize_false_values() {
 		return false;
 	}
 
+	/**
+	 * Sanitize number.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return float|int|string
+	 */
 	public static function sanitize_number( $input, $setting ) {
 		$input_attrs = $setting->manager->get_control( $setting->id )->input_attrs;
 		if ( isset( $input_attrs ) ) {
@@ -122,25 +193,59 @@ class Sanitize {
 		return is_numeric( $input ) ? $input : $setting->default;
 	}
 
+	/**
+	 * Sanitize email.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return string
+	 */
 	public static function sanitize_email( $input, $setting ) {
 		$email = sanitize_email( $input );
 		return ! is_null( $email ) ? $email : $setting->default;
 	}
 
+	/**
+	 * Sanitize URL.
+	 *
+	 * @param $input
+	 * @return string
+	 */
 	public static function sanitize_url( $input ) {
 		return esc_url_raw( $input );
 	}
 
+	/**
+	 * Sanitize dropdown categories.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return int
+	 */
 	public static function sanitize_dropdown_categories( $input, $setting ) {
 		$input = absint( $input );
 		return term_exists( $input, 'category' ) ? $input : $setting->default;
 	}
 
+	/**
+	 * Sanitize dropdown pages.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return int
+	 */
 	public static function sanitize_dropdown_pages( $input, $setting ) {
 		$input = absint( $input );
 		return 'publish' === get_post_status( $input ) ? $input : $setting->default;
 	}
 
+	/**
+	 * Sanitize image upload.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return mixed|string
+	 */
 	public static function sanitize_image_upload( $input, $setting ) {
 		$mimes = array(
 			'jpg|jpeg|jpe' => 'image/jpeg',
@@ -154,6 +259,13 @@ class Sanitize {
 		return $file['ext'] ? $input : '';
 	}
 
+	/**
+	 * Sanitize background.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return array
+	 */
 	public static function sanitize_background( $input, $setting ) {
 		if ( ! is_array( $input ) ) {
 			return array();
@@ -180,14 +292,24 @@ class Sanitize {
 		return $output;
 	}
 
+	/**
+	 * Sanitize typography.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return mixed
+	 */
 	public static function sanitize_typography( $input, $setting ) {
 		return $input;
 	}
 
-	public static function sanitize_gradient( $input ) {
-		return ! is_array( $input ) ? $input : array();
-	}
-
+	/**
+	 * Sanitize sortable.
+	 *
+	 * @param $input
+	 * @param WP_Customize_Setting $setting
+	 * @return array
+	 */
 	public static function sanitize_sortable( $input, $setting ) {
 		$choices    = $setting->manager->get_control( $setting->id )->choices;
 		$input_keys = $input;

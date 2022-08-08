5bcbd04bce4c084817e45c6faf0fbe0133a6a269
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-08-08 10:26:54 +0545

    Add fontawesome gulp task

--- customind/gulpfile.js
+++ customind/gulpfile.js
@@ -18,3 +18,22 @@ gulp.task( 'fetch:google-fonts', () => {
 		}
 	} );
 } );
+
+gulp.task( 'fetch:fontawesome', () => {
+	return request( 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/metadata/icons.json', function( error, response, body ) {
+		if ( ! error && response.statusCode === 200 ) {
+			const icons = JSON.parse( body );
+			const newIcons = Object.keys( icons ).reduce( function( acc, crr ) {
+				const svg = icons[ crr ].svg?.brands || icons[ crr ].svg?.solid;
+				acc[ crr ] = `<svg class="${ crr }" xmlns="http://www.w3.org/2000/svg" viewBox="${ svg.viewBox.join( ' ' ) }"><path fill="currentColor" d="${ svg.path }"></path></svg>`;
+				return acc;
+			}, {} );
+			fs.writeFile( 'src/controls/icons.json', JSON.stringify( newIcons, null, 2 ), function( err ) {
+				if ( ! err ) {
+					// eslint-disable-next-line no-console
+					console.log( 'Fontawesome library updated!' );
+				}
+			} );
+		}
+	} );
+} );

a4b57b39d78580a700d4c86b6950c15c66673a6b
Ashesh Magar <45454049+asheshmagar@users.noreply.github.com>
2022-08-23 17:59:15 +0545

    Register sidebar and menu

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
-require __DIR__ . '/customind/customind.php';
+require __DIR__ . '/core/init.php';
 require __DIR__ . '/customizer/options.php';
 
+register_nav_menu( 'primary', 'Primary' );
+register_sidebar(
+	array(
+		'id'   => 'sidebar-1',
+		'name' => 'Sidebar',
+	)
+);
